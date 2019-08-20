3e03abf2e33b9ffe00fa77a8b63e62f5ebeaf3a6
Игорь Хан <korki123@mail.ru>
2019-08-20 19:15:19 +0300

    Внес правки. Заработала модалка на главной

--- source/js/app.js
+++ source/js/app.js
@@ -1,40 +1,33 @@
-// var chart = document.querySelector(".catalog__price");
-// var modalShow = document.querySelector(".modal");
-//
-// chart.addEventListener("click", function (evt) {
-//
-//   evt.preventDefault();
-//   // console.log ("нажали на корзинку");
-//
-//   modalShow.classList.add("modal-show");
-//
-// });
 
-// var modalPush = querySelector(".modal-overlay");
-// var modalPush = querySelector(".modal__button");
-// var modalClose = querySelector(".modal-show");
-//
-// modalPush.addEventListener("click", funtion() {
-//
-//   modalClose.classList.remove("modal-show");
-//
-// });
+var order = document.querySelector('.product-week__button');
+var modalShow = document.querySelector('.modal');
+var overlay = document.querySelector('.modal-overlay');
+var charts = document.querySelectorAll('.catalog__price');
 
-// charts.forEach((chart) => {
-//   chart.addEventListener("click", function (evt) {
-//
-//     evt.preventDefault();
-//     console.log ("нажали на корзинку");
-//   });
-// })
+order.addEventListener('click', function (evt) {
 
-var charts = document.querySelectorAll(".catalog__price");
+  evt.preventDefault();
+  console.log('нажали на корзинку');
+
+  modalShow.classList.add('modal-show');
+});
+
+
+
+overlay.addEventListener('click', function (evt) {
+
+  evt.preventDefault();
+  console.log('нажали на корзинку');
+
+  modalShow.classList.remove('modal-show');
+});
+
 
 charts.forEach((chart) => {
-  chart.addEventListener("click", function (evt) {
 
-    evt.preventDefault();
-    console.log ("нажали на корзинку");
+  chart.addEventListener('click', function (evt) {
+
+    modalShow.classList.add('modal-show');
 
   });
-})
+});
