3d432fad6c9db18e0f4bd748fde6cb5ba1502fd5
Игорь Хан <korki123@mail.ru>
2019-08-25 15:14:25 +0300

    готово все до include

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,13 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var sprite = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -16,6 +23,10 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
@@ -35,3 +46,40 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("imagemin", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
+]))
+  .pipe(gulp.dest(source/img))
+});
+
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+
+  .pipe(gulp.dest("source/img"))
+});
+
+gulp.task("sprite", function(){
+  return gulp.src("source/img/sprite-*.svg")
+
+  .pipe(sprite({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+
+  .pipe(gulp.dest("source/img"))
+});
+
+galp.task("thtml", function() {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([include()]))
+
+  pipe.(gulp.dest("source"));
+})
