1a0f9f2538a9adcb6f411f089ea1ef6c6ddaad3b
Игорь Хан <korki123@mail.ru>
2019-08-26 13:56:33 +0300

    Вроде бы вся автоматизация работает

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var webp = require("gulp-webp");
 var sprite = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -28,22 +29,28 @@ gulp.task("css", function () {
     .pipe(rename("style.min.css"))
 
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+    server: "build/"
+    // notify: false,
+    // open: true,
+    // cors: true,
+    // ui: false
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/sprite-*.svg", gulp.series("sprite", "posthtml", "refresh"));
+  gulp.watch("source/*.html", gulp.series("posthtml", "refresh"));
 });
+
+gulp.task("refresh", function(done) {
+  server.reload();
+  done();
+})
 
 gulp.task("imagemin", function() {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
@@ -72,7 +79,7 @@ gulp.task("sprite", function(){
   }))
   .pipe(rename("sprite.svg"))
 
-  .pipe(gulp.dest("source/img"))
+  .pipe(gulp.dest("build/img"))
 });
 
 gulp.task("posthtml", function() {
@@ -82,11 +89,11 @@ gulp.task("posthtml", function() {
     include()
   ]))
 
-  .pipe(gulp.dest("source"))
+  .pipe(gulp.dest("build"))
 });
 
 
-gulp.task("copy", function(){
+gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff, woff2}",
     "source/img/**",
@@ -97,5 +104,9 @@ gulp.task("copy", function(){
   .pipe(gulp.dest("build"));
 });
 
-gulp.task("build", gulp.series("css", "sprite", "posthtml"));
+gulp.task ("clean", function(){
+  return del("build");
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "posthtml"));
 gulp.task("start", gulp.series("build", "server"));
